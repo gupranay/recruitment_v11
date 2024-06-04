3d3de3a519ec9fb50e50e4272162b8e2657d1dd3
gupranay <gupranay@umich.edu>
2024-06-03 23:09:28 -0400

    THEME

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -36,5 +36,5 @@ export async function GET(request: Request) {
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  return NextResponse.redirect(`${HOSTNAME}/auth/auth-code-error`)
 }
