eee25c4715d535f66c57865dece3d81f4f08c733
gupranay <gupranay@umich.edu>
2025-01-23 18:23:59 -0500

    paths

--- lib/constant/inedx.ts
+++ lib/constant/inedx.ts
@@ -1,2 +1,2 @@
- export const protectedPaths = ["/dashboard", "/profile", "/newdash", "/dash", "/anonymous", "/pages/anonymous/[slug].tsx", "/anonymous/[slug]", "/anonymous/58uvre?slug=58uvre", "/app/anonymous"];
+ export const protectedPaths = ["/dashboard", "/profile", "/newdash", "/dash", "/anonymous", "/pages/anonymous/[slug].tsx", "/anonymous/[slug]", "/anonymous/58uvre?slug=58uvre", "/app/anonymous", "/build", "/app/build"];
  export const authPaths = ["/register", "/signin", "/auth"];
