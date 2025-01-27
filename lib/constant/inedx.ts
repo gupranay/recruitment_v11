d95c0d41da235bab0eaf561099879be86d34cf0f
Pranay Gupta <115174521+gupranay@users.noreply.github.com>
2025-01-27 16:11:55 -0500

    Update inedx.ts

--- lib/constant/inedx.ts
+++ lib/constant/inedx.ts
@@ -1,2 +1,2 @@
- export const protectedPaths = ["/dashboard", "/profile", "/newdash", "/dash", "/anonymous", "/pages/anonymous/[slug].tsx", "/anonymous/[slug]", "/anonymous/58uvre?slug=58uvre", "/app/anonymous", "/build", "/app/build"];
+ export const protectedPaths = ["/dashboard", "/profile", "/newdash", "/dash", "/anonymous", "/pages/anonymous/[slug].tsx", "/anonymous/[slug]", "/anonymous/58uvre?slug=58uvre", "/app/anonymous", "/build", "/app/build", "/cc", "/app/cc"];
  export const authPaths = ["/register", "/signin", "/auth"];
